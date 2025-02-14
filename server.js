6d3cc5573dc1885f59880469469fc8eec860b8d8
Thodoris Theodoridis <theotheodoridis98@gmail.com>
2025-02-14 16:27:15 +0200

    mongoose connect and dotenv

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-const port = 5000;
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 const app = express();
+const connectDB = require('./config/db');
+
+connectDB();
 
 /** 
 .use() method is used to mount the specified middleware function(s) at the path which is being specified.
